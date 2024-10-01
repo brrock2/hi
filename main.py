91d44feaad24e20cd081b72e1437221207987c53
brrock2 <167097666+brrock2@users.noreply.github.com>
2024-10-01 14:32:04 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 def on_forever():
-    music.play(music.string_playable("", 120), music.PlaybackMode.UNTIL_DONE)
+    basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    """)
+    basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    """)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
 basic.forever(function on_forever() {
-    music.play(music.stringPlayable("", 120), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+    basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 })
